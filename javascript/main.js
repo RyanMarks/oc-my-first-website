045b220b134f9fd5abee790c35d768a557a3ba3d
RyanMarks <RyanMarks@student.olympic.edu>
2026-02-18 23:44:15 -0800

    assignment. Page doesn't look the greatest rn but it's technicaly interactive. Will fix how the CSS looks later

--- javascript/main.js
+++ javascript/main.js
@@ -2,5 +2,19 @@ fetch("components/nav.html")
 .then(response => response.text())
 .then(data => {
     document.getElementById("nav-bar-placeholder").innerHTML = data;
+
+    const hamburger = document.getElementById("hamburger");
+    const navList = document.getElementById("navbar");
+
+    hamburger.addEventListener("click", () => {
+        navList.classList.toggle("active");
+    });
 })
 .catch(error => console.error("failed to load nav", error));
+
+fetch("components/linkbox.html")
+.then(response => response.text())
+.then(data => {
+    document.querySelectorAll(".link-box-placeholder").innerHTML = data;
+})
+.catch(error => console.error("failed to load linkbox", error));
--- javascript/parallax.js
+++ javascript/parallax.js
@@ -1,4 +1,6 @@
 window.addEventListener("scroll",() => {
     const scrollPosition =window.scrollY;
-    document.body.style.backgroundPositionY = scrollPosition * -.5 + "px"
+    document.getElementById("background1").style.backgroundPositionY = scrollPosition * 2 + "px";
+    document.getElementById("background2").style.backgroundPositionY = scrollPosition * 1 + "px";
+    document.getElementById("background3").style.backgroundPositionY = scrollPosition * .5 + "px";
 })

35f11634573d028709adca760a08b71dba1bf69f
RyanMarks <RyanMarks@student.olympic.edu>
2026-03-04 16:47:22 -0800

    Feedback

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,16 @@
 fetch("components/nav.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("nav-bar-placeholder").innerHTML = data;
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("nav-bar-placeholder").innerHTML = data;
 
-    const hamburger = document.getElementById("hamburger");
-    const navList = document.getElementById("navbar");
+        const hamburger = document.getElementById("hamburger");
+        const navList = document.getElementById("navbar");
 
-    hamburger.addEventListener("click", () => {
-        navList.classList.toggle("active");
-    });
-})
-.catch(error => console.error("failed to load nav", error));
+        hamburger.addEventListener("click", () => {
+            navList.classList.toggle("active");
+        });
+    })
+    .catch(error => console.error("failed to load nav", error));
 
 fetch("components/linkbox.html")
 .then(response => response.text())
@@ -23,8 +23,8 @@ fetch("components/linkbox.html")
 .catch(error => console.error("failed to load linkbox", error));
 
 fetch("components/footer.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("footer-placeholder").innerHTML = data
-})
-.catch(error => console.error("failed to load footer", error));
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data
+    })
+    .catch(error => console.error("failed to load footer", error));
